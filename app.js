dd9d6071382467634545549456adc93f54d840fb
travelingdoodle <reggiebrown@gmail.com>
2017-08-02 19:08:32 -0500

    firebase code added

---
+++ app.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2P3JURJEZcaG1PzDlOnYilV2KsQBHsvA",
+    authDomain: "timesheet-c070b.firebaseapp.com",
+    databaseURL: "https://timesheet-c070b.firebaseio.com",
+    projectId: "timesheet-c070b",
+    storageBucket: "",
+    messagingSenderId: "763391736641"
+  };
+  firebase.initializeApp(config);
+
+  
